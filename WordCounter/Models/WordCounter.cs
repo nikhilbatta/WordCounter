ab4e39b09849ce22295f46e0ea374692a2f82503
nikhilbatta <nbatta08@gmail.com>
2019-10-04 10:43:59 -0700

    takes user input and splits it into an array of strings

---
+++ WordCounter/Models/WordCounter.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace WordCounter
+{
+    public class Calculation
+    {
+        public static List<string> AuthorList = new List<string>();  
+        
+        public static void userInputToList(string userInput)
+        {
+            string [] userinputostring = userInput.Split(" ");
+           
+           
+        }
+    }
+}
---
+++ WordCounter/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace WordCounter
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Calculation.userInputToList("I am a dog");
+            
+            
+        }
+    }
+}
