93481630456612a1d8c646a9b18ee153021a2481
nikhilbatta <nbatta08@gmail.com>
2019-10-04 13:45:53 -0700

    seperation of logic by grab word to grab setntence to catch errors

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -5,9 +5,41 @@ namespace WordCounter
 {
     public class Calculation
     {
-        // public static List<string> AuthorList = new List<string>();  
-        
-        public static int userStringToCounter(string whatitshouldbe, string userInputSentence)
+        public static dynamic GrabWord()
+        {
+            Console.WriteLine("Please enter in a word that you would like to find in the sentence ");
+            string userFirstAnswer = Console.ReadLine();
+            char[] errorChecker = userFirstAnswer.ToCharArray();
+            foreach(char error in errorChecker)
+            {
+                if(Char.IsPunctuation(error))
+                {
+                    string errormessage = "please enter in something valid";
+                    GrabWord();
+                    return errormessage;
+                }
+            }
+            return GrabSentence(userFirstAnswer);
+        }
+        public static int GrabSentence(string userFirstAnswer)
+        {
+            
+            Console.WriteLine("please enter in a sentence");
+            string userSecondAnswer = Console.ReadLine();
+            char[] secondAnswerErrorChecker = userSecondAnswer.ToCharArray();
+            foreach(char error in secondAnswerErrorChecker)
+            {
+                if(Char.IsPunctuation(error))
+                {
+                    Console.WriteLine("please enter in a valid sentence");
+                    GrabSentence(userFirstAnswer);
+                }
+            }
+            int counter = Calculation.userStringToCounter(userFirstAnswer,userSecondAnswer);
+            return counter;
+
+        }
+          public static int userStringToCounter(string whatitshouldbe, string userInputSentence)
         {
             string [] stringList = userInputSentence.ToLower().Split(" ");
             int counter = 0;
--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -7,32 +7,8 @@ namespace WordCounter
     {
         public static void Main()
         {
-            // Calculation.userInputToList("dog", "I am a dog doghead dog");
-            // foreach(string item in Calculation.AuthorList)
-            // {
-            //     Console.WriteLine(item);
-            // }
-            StartWordCounter();
-            
+            Console.WriteLine(Calculation.GrabWord());
         }
-        public static void StartWordCounter()
-        {
-            Console.WriteLine("Please enter in a word that you would like to find in the sentence ");
-            string userFirstAnswer = Console.ReadLine();
-            char[] errorChecker = userFirstAnswer.ToCharArray();
-            foreach(char error in errorChecker)
-            {
-                if(Char.IsPunctuation(error))
-                {
-                    Console.WriteLine("please enter in a valid word");
-                    StartWordCounter();
-                }
-            }
-            Console.WriteLine("please enter in a sentence");
-            string userSecondAnswer = Console.ReadLine();
-           
-            Console.WriteLine(Calculation.userStringToCounter(userFirstAnswer,userSecondAnswer));
-
-        }
+        
     }
 }
